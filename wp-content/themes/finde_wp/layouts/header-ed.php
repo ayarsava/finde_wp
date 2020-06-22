a3be07ea3b02811c837fd673e82b7cbcaeb8ea7a
Ayar Sava <ayarsava@gmail.com>
2020-06-22 13:48:05 -0300

    REV:Modifico color en header

--- wp-content/themes/finde_wp/layouts/header-ed.php
+++ wp-content/themes/finde_wp/layouts/header-ed.php
@@ -95,7 +95,7 @@
 							<a class="nav-link" href="/videojuegos" style="color: #009aae!important">Edición Videojuegos</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="/musica" style="color: ##793d97!important">Edición Música</a>
+							<a class="nav-link" href="/musica" style="color: #793d97!important">Edición Música</a>
 						</li>
 			    	</ul>
 			    	<div class="d-flex bd-highlight">
--- wp-content/themes/finde_wp/layouts/header-vj.php
+++ wp-content/themes/finde_wp/layouts/header-vj.php
@@ -95,7 +95,7 @@
 							<a class="nav-link" href="/editorial" style="color: #ff677d!important">Edición Editorial</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="/musica" style="color: ##793d97!important">Edición Música</a>
+							<a class="nav-link" href="/musica" style="color: #793d97!important">Edición Música</a>
 						</li>
 			    	</ul>
 			    	<div class="d-flex bd-highlight">
