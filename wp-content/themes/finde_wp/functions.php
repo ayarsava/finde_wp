900150b96dc3b52c6380b616250effd4a94faeff
Ayar Sava <ayar.sava@cultura.gob.ar>
2020-10-22 15:22:03 -0300

    REV:Cambio version de css para saltar cache

--- wp-content/themes/finde_wp/functions.php
+++ wp-content/themes/finde_wp/functions.php
@@ -162,7 +162,7 @@ function finde_wp_scripts() {
 
 	wp_enqueue_script( 'finde_wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array(), '1.0', true );
+	wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array(), '1.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
