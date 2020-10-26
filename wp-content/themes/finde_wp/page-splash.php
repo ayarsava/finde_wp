9e07fb4775522b2fdbe6f89156ad4103ca290633
Ayar Sava <ayar.sava@cultura.gob.ar>
2020-10-26 15:12:54 -0300

    REV:Cargo splash audiovisual

--- wp-content/themes/finde_wp/page-splash.php
+++ wp-content/themes/finde_wp/page-splash.php
@@ -61,6 +61,21 @@
 			</div>
 			<div class="col-lg-12 col-md-15 mx-auto">
 				<div class="card-deck my-1 text-center">
+					<div class="w-100">
+						<div class="w-50 bg-transparent mb-4 mx-auto">
+							<div class="card text-center shadow hover-shadow">
+									<div class="py-3 col-md-8 mx-auto">
+											<img src="/wp-content/themes/finde_wp/assets/img/icon-audiovisual.png" class="img-fluid icon">
+									</div>
+									<div class="pb-1 text-white">
+										<h3><strong>PRÓXIMA EDICIÓN<br>AUDIOVISUAL</strong></h3>
+										<p>Del 05 AL 08 DE NOVIEMBRE</p>
+										<div class="mb-3" id="activo" style="display:none;"><a href="/audiovisual" class="btn btn-outline-light" title="Accedé a la Edición de Audiovisual">Accedé</a></div>
+										<button id="soon" type="button" class="btn btn-outline-light mb-3" disabled style="display:none;">Muy pronto</button>
+									</div>
+							</div>
+						</div>
+					</div>
 					<div class="w-50 bg-transparent mb-4 mx-auto">
 						<div class="card text-center shadow hover-shadow">
 								<div class="py-3 col-md-8 mx-auto">
@@ -69,23 +84,24 @@
 								<div class="pb-1 text-white">
 									<h3><strong>EDICIÓN VIDEOJUEGOS</strong></h3>
 									<p>Del 8 AL 11 DE OCTUBRE</p>
-									<div class="mb-3"><a href="/videojuegos" title="Accedé a la Edición de Videojuegos" class="btn btn-outline-light">Revivíla</a></div>
+									<div class="mb-3"><a href="/videojuegos" title="Accedé a la Edición de Videojuegos" class="btn btn-outline-light">Revivila</a></div>
 								</div>
 						</div>
 					</div>
 					<div class="w-50 bg-transparent mb-4 mx-auto">
 						<div class="card text-center shadow hover-shadow">
 								<div class="py-3 col-md-8 mx-auto">
-										<img src="/wp-content/themes/finde_wp/assets/img/icon-vj.png" class="img-fluid icon">
+										<img src="/wp-content/themes/finde_wp/assets/img/icon-diseno.png" class="img-fluid icon">
 								</div>
 								<div class="pb-1 text-white">
-									<h3><strong>PRÓXIMA EDICIÓN DISEÑO</strong></h3>
+									<h3><strong>EDICIÓN DISEÑO</strong></h3>
 									<p>Del 22 AL 25 DE OCTUBRE</p>
-									<div class="mb-3" id="activo" style="display:none;"><a href="/diseno" class="btn btn-outline-light" title="Accedé a la Edición de Diseño">Accedé</a></div>
-									<button id="soon" type="button" class="btn btn-outline-light mb-3" disabled style="display:none;">Muy pronto</button>
+									<div class="mb-3"><a href="/diseno" title="Accedé a la Edición de Diseño" class="btn btn-outline-light">Revivila</a></div>
 								</div>
 						</div>
 					</div>
+
+					
 				</div>
 				
 			</div>
@@ -103,7 +119,7 @@
 			hour = minute * 60,
 			day = hour * 24;
 
-			let countDown = new Date('2020-10-22T20:00:00'.replace(/\s/, 'T')).getTime(),
+			let countDown = new Date('2020-11-05T15:00:00'.replace(/\s/, 'T')).getTime(),
 			x = setInterval(function() {    
 
 			let now = new Date().getTime(),
