eba220c24d8de657755f5bf9618af4d3c220b7da
Ayar Sava <ayar.sava@cultura.gob.ar>
2020-11-19 17:23:46 -0300

    REV:Cambio slide

--- wp-content/themes/finde_wp/page-editorial.php
+++ wp-content/themes/finde_wp/page-editorial.php
@@ -17,22 +17,19 @@ get_template_part( 'layouts/header', 'ed' );
     <div id="content" class="editorial">
         <section id="slide-principal">
             <div id="carouselCaptions" class="carousel slide" data-ride="carousel">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <?php wp_showSlides_fullbg_portag('editorial') ?>
+                </div>
 
-              <!-- Wrapper for slides -->
-              <div class="carousel-inner">
-                <?php wp_showSlides_fullbg_editorial() ?>
-              </div>
-
-              <!-- Controls -->
-              <!-- Carousel nav -->
-              <a class="carousel-control-prev" href="#carouselCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+                <a class="carousel-control-prev" href="#carouselCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </section>
 
